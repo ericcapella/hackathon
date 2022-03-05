9a255888f0303ade7703d551a4baa8cce246a6ad
Eric <eric.capella.1r@gmail.com>
2022-03-05 13:06:26 +0100

    consume de express-server

--- packages/express-server/src/main.ts
+++ packages/express-server/src/main.ts
@@ -2,6 +2,8 @@ import * as express from 'express';
 import * as cors from 'cors';
 import AppRouter from "./app/routes";
 import {connectToDatabase} from "./app/mongo";
+import { configKafkaClient, consumeMessages, KafkaConsumer, topicInitialization, TweetSentimentEvent } from '@hackathon1021/kafka';
+import { Tweet } from './app/models/tweet.model';
 const app = express();
 app.use(express.json())
 app.use(cors());
@@ -15,12 +17,38 @@ const start = async () => {
 
   await connectToDatabase();
 
+
+  const kafka = configKafkaClient("express-server");
+
+  const consumer:KafkaConsumer<TweetSentimentEvent> = async (kafka, event: TweetSentimentEvent) => {
+    const analyzedTweet = new Tweet(event)
+    //TODO asegurarnos que event es un objecto con un solo tweet
+
+    await analyzedTweet.save();
+
+  };
+
+  topicInitialization(kafka, ["analyzed-tweets"]);
+
+
+  consumeMessages(kafka, "express-server", "analyzed-tweets", consumer);
+
   app.listen(3001, () => {
     console.log(`Listening at http://localhost:${3001}`);
   });
 
 
-  
+
+  // const kafka = configKafkaClient("sentiment-analyzer");
+
+  // const consumer:KafkaConsumer<TweetEvent> = (kafka, event: TweetEvent) => {
+  //   const sentiment = analyzeSentiment(event.text);
+  //   sendMessage(kafka, "analyzed-tweets", [{value:JSON.stringify({...event, sentiment})}]);
+  // };
+
+  // topicInitialization(kafka, ["tweet-events", "analyzed-tweets"]);
+
+  // consumeMessages(kafka, "sentiment-analyzer", "tweet-events", consumer);  
 
 }
 
